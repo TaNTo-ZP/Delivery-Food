c1ade5e19e81801435aa24ec59f6142b15035c0d
Oleh <103416414+TaNTo-ZP@users.noreply.github.com>
2022-12-22 16:34:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,100 @@
+// ** Hamburger
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const body = document.body;
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
+
+// ** UpArrow
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// ** Tabs
+
+function myFunction() {
+  let x = document.getElementById("myDIV");
+  if (x.innerHTML === "HeadNulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Pus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.ing") {
+    x.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, maxime! Ducimus tempore eveniet in et! Sint hic rerum facere voluptas dolorem nobis omnis aliquam quasi molestiae repellendus perferendis veritatis quas perspiciatis eaque ipsa quaerat explicabo, aliquid non accusantium animi iusto!";
+  } else {
+    x.innerHTML = "HeadNulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Pus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.ing";
+  }
+}
+
+
+// ** Slider
+
+$(function() {
+
+  $('.rev__slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
+})
